9af50b54418b9dbddb09c8397302ff5636b01ebb
Sebosun <sebos@protonmail.com>
2021-08-03 00:42:31 +0200

    audio sorta plays, its 2 youtubes layered on top of each other

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import YoutubeEmbed from "./components/YoutubeEmbed";
 function App() {
   return (
     <div className="modal">
+      <YoutubeEmbed audio={true} embedId="5yx6BWlEVcY" />
       <YoutubeEmbed embedId="0nTO4zSEpOs" />
     </div>
   );
--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./YoutubeEmbed.css";
 
-const YoutubeEmbed = ({ embedId }) => {
+const YoutubeEmbed = ({ embedId, audio }) => {
+  // const [isMuted, setIsMuted] = useState(false);
+
   return (
     <div className="video-background">
       <div className="video-foreground">
         <iframe
-          src={`https://www.youtube.com/embed/${embedId}?autoplay=1&mute=1&controls=0&start=13&playsinline=1&rel=0&showinfo=0&enablejsapi=1&widgetid=1`}
+          src={`https://www.youtube.com/embed/${embedId}?autoplay=1${
+            audio ? "" : "&mute=1"
+          }&controls=0&start=13&playsinline=1&rel=0&showinfo=0&enablejsapi=1&widgetid=1`}
           frameBorder="0"
           allowFullScreen="1"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           title="Embedded youtube"
           width="100%"
           height="100%"
