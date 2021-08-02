3f0798c1111852f90acd9d93e97be11e606ce0d0
Sebosun <sebos@protonmail.com>
2021-08-03 00:51:12 +0200

    display none on audio

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import YoutubeEmbed from "./components/YoutubeEmbed";
 function App() {
   return (
     <div className="modal">
-      <YoutubeEmbed audio={true} embedId="5yx6BWlEVcY" />
       <YoutubeEmbed embedId="0nTO4zSEpOs" />
+      <YoutubeEmbed audio={true} embedId="5yx6BWlEVcY" />
     </div>
   );
 }
