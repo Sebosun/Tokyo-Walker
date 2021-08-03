3763323f8d9f74061a1c6e9d363938d545ae1a9e
Sebosun <sebos@protonmail.com>
2021-08-03 19:14:53 +0200

    replacing my component with react player

--- src/components/Wrappers/Video.js
+++ src/components/Wrappers/Video.js
@@ -1,11 +1,28 @@
 import YoutubeEmbed from "../Youtube/YoutubeEmbed";
 import VideoContext from "../../store/video-context";
+import ReactPlayer from "react-player";
+import "../Youtube/YoutubeEmbed.css";
 import { useContext } from "react";
 
 const Video = () => {
   const videoCtx = useContext(VideoContext);
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const [isMuted, setIsMuted] = useState(true);
   console.log(videoCtx);
-  return <div class="video">{<YoutubeEmbed embedId={videoCtx.url} />}</div>;
+  return (
+    <>
+      <div className="video-background">
+        <div className="video-foreground">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${videoCtx.url}`}
+            muted={true}
+            playing={true}
+          />
+        </div>
+      </div>
+      {/* <div class="video">{<YoutubeEmbed embedId={videoCtx.url} />}</div>; */}
+    </>
+  );
 };
 
 export default Video;
