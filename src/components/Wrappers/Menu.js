9405a7cbdc0e51743b7a14debdaf0041db544a9e
Sebosun <sebos@protonmail.com>
2021-08-04 13:59:12 +0200

    wider menu, fix for firefox

--- src/components/Wrappers/Menu.js
+++ src/components/Wrappers/Menu.js
@@ -7,25 +7,31 @@ import MusicMenu from "../menu/MusicMenu";
 import Button from "../ui/Button";
 import classes from "./Menu.module.css";
 import { FiGithub } from "react-icons/fi";
+
 const Menu = () => {
   const videoCtx = useContext(VideoContext);
   const audioCtx = useContext(AudioContext);
+
   return (
     <div className={classes.menu}>
       <div className={classes.video}>
         <VideoList />
       </div>
+
       <Button onClick={videoCtx.changeMute}>
         {videoCtx.muted
           ? "Turn On The Street Noise"
           : "Turn Off The Street Noise"}
       </Button>
+
       <div className="music">
         <MusicMenu />
       </div>
+
       <Button onClick={audioCtx.setPlayStatus}>
         {`  ${audioCtx.playing ? "Stop music" : "Play music"} `}
       </Button>
+
       <a
         href="https://github.com/Sebosun/Tokyo-Walker"
         className={classes.github}
@@ -35,4 +41,5 @@ const Menu = () => {
     </div>
   );
 };
+
 export default Menu;
