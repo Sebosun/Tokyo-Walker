2c79b9672a9378b2cf44d238c568322995faec7c
Sebosun <sebos@protonmail.com>
2021-08-04 15:46:14 +0200

    actually adding the volume range

--- src/components/menu/MusicMenu.js
+++ src/components/menu/MusicMenu.js
@@ -1,11 +1,15 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import AudioContext from "../../store/audio-context";
-import Button from "../ui/Button";
+import VolumeControl from "../ui/VolumeControl";
 import classes from "./MusicMenu.module.css";
 import music from "../../data/music.json";
 
-const MusicMenu = (props) => {
+const MusicMenu = () => {
   const audioCtx = useContext(AudioContext);
+
+  const onVolumeChange = (e) => {
+    audioCtx.changeVolume(e.target.value);
+  };
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -15,16 +19,15 @@ const MusicMenu = (props) => {
     const lofiLenght = music.lofi.length;
     audioCtx.changeAudio(music.lofi[getRandomInt(lofiLenght)]);
   };
+
   return (
     <div>
       <div style={{ fontSize: "10px" }} className={classes.musicNavigation}>
-        {/* <Button>prev</Button> */}
         <div onClick={selectRandom} className={classes.name}>
           {audioCtx.name}
         </div>
-        {/* {<Button> console.log(music.lofi.length)}>next</Button>} */}
       </div>
-      {/* <a href={audioCtx.url}>Source link</a> */}
+      <VolumeControl onVolumeChange={onVolumeChange} volume={audioCtx.volume} />
     </div>
   );
 };
