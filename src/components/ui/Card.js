a509995d9826907d1d7c0b56841484c2ddcf1ab6
Sebosun <sebos@protonmail.com>
2021-08-05 01:17:42 +0200

    typo fix

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,6 +1,10 @@
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-  return <div className={classes.Card}>{props.children}</div>;
+  return (
+    <div onClick={props.onClick} role={props.role} className={classes.Card}>
+      {props.children}
+    </div>
+  );
 };
 export default Card;
