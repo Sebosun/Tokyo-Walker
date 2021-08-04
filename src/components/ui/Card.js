0f3019db23443c184a357f6617068061449cf4d0
Sebosun <sebos@protonmail.com>
2021-08-05 01:33:03 +0200

    slight borders

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -2,7 +2,11 @@ import classes from "./Card.module.css";
 
 const Card = (props) => {
   return (
-    <div onClick={props.onClick} role={props.role} className={classes.Card}>
+    <div
+      onClick={props.onClick}
+      role={props.role}
+      className={`${classes.Card} ${props.className && props.className}`}
+    >
       {props.children}
     </div>
   );
