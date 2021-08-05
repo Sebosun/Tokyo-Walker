9142c195fc942813b2e8fc578c9f3a493635bc97
Sebosun <sebos@protonmail.com>
2021-08-05 14:12:08 +0200

    able to change the country for videos

--- src/components/menu/VideoList.js
+++ src/components/menu/VideoList.js
@@ -1,15 +1,35 @@
-import Videos from "../../data/videos.json";
+import videos from "../../data/videos.json";
 import VideoContext from "../../store/video-context";
 import classes from "./VideoList.module.css";
 
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 
 const VideoList = () => {
+  const [currentCountry, setCurrentCountry] = useState("japan");
+  const [videosList, setVideosList] = useState([]);
   const videoCtx = useContext(VideoContext);
+
   const changeDisplay = (item) => {
     videoCtx.addVideo(item);
   };
-  const list = Videos.japan.map((item) => {
+
+  const changeCountry = (country) => {
+    setCurrentCountry(country);
+  };
+
+  const videoCategoriesGen = () => {
+    let content = [];
+    for (let property in videos) {
+      content.push(
+        <div onClick={() => changeCountry(property)} key={property}>
+          {property}
+        </div>
+      );
+    }
+    return content;
+  };
+
+  const list = videos[currentCountry].map((item) => {
     return (
       <li key={item.url}>
         <div role="button" onClick={() => changeDisplay(item)}>
@@ -19,9 +39,12 @@ const VideoList = () => {
     );
   });
   return (
-    <div className={classes.video}>
-      <ul>{list}</ul>
-    </div>
+    <>
+      <div className={classes.video}>
+        <ul>{list}</ul>
+      </div>
+      {videoCategoriesGen()}
+    </>
   );
 };
 export default VideoList;
