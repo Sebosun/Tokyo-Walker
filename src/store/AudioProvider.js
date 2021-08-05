f6374dd024a600426b016cfc66466be8a3b9e652
Sebosun <sebos@protonmail.com>
2021-08-05 23:07:56 +0200

    back to prev default music

--- src/store/AudioProvider.js
+++ src/store/AudioProvider.js
@@ -5,9 +5,8 @@ import AudioContext from "./audio-context";
 const AudioProvider = (props) => {
   const [audioState, setAudioState] = useState({
     name: "Toonorth Aftersome",
-    url: "https://soundcloud.com/user-405263843/arakawa-under-the-bridge-op-full-ver-etsuko-yakushimaru-venus-to-jesus",
+    url: "https://soundcloud.com/chillhopdotcom/toonorth-aftersome",
   });
-  // url: "https://soundcloud.com/chillhopdotcom/toonorth-aftersome",
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(50);
   const [genre, setGenre] = useState("lofi");
